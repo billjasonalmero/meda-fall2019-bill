a32fb30c9e447aa3d021380d0fc6365e3f1c2098
Bill Jason <jasonez_03@yahoo.com>
2019-10-04 12:50:17 -0700

    upadted arrays.js + arrays & loops

--- practice/javascript/arrays.js
+++ practice/javascript/arrays.js
@@ -53,3 +53,35 @@ console.log(weekNames);
 var day = weekNames.pop(); // removes the "Sunday"
 weekNames.unshift(day);
 console.log(weekNames);
+//weekNames.unshift(  (weekNames.pop()  );
+//End Challenge
+
+
+//Array Method Splice(), designed to extract a part of an array
+
+weekNames.splice(3, 1); //start with index 3 and remove 1 element
+console.log(weekNames);
+
+//Add values to array manually by specifying the index number
+var splitArray = weekNames.splice(3, 2); //start from index 3 and remove 2 elements
+
+//Update values to array by specifiying theindex index number
+console.log(splitArray);
+
+//Assign a value to an index thst doesn't exit yet
+splitArray[10] = "Hiddenday";
+
+console.log(splitArray);
+
+//Arrays and Loops
+var names = ["Alpha", "Sam", "Max", "Georgio", "Giermo", "Horacio", "Batman", "Bill", "Mackaw", "Robin"];
+
+//or use counter++
+for(var counter = 0; counter<10; counter = counter +1) {
+  console.log("Hello " + names[counter] + "!");
+}
+//or
+                                  //instead of 10, we can use .lenght so we always loop for the exact number of items of the names array
+for(var counter = 0; counter<names.length; counter = counter +1) {
+  console.log("Hello " + names[counter] + "!");
+}
